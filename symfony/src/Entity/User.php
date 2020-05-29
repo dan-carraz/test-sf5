d750db57c7b8f55320ad1d4d31bb47a603172609
dan <dan.carraz@gmail.com>
2020-05-29 16:57:27 +0200

    add doctrine

---
+++ symfony/src/Entity/User.php
@@ -0,0 +1,92 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\UserRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=UserRepository::class)
+ */
+class User
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private ?int $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private ?string $firstName;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private ?string $lastName;
+
+    /**
+     * @ORM\Column(type="date")
+     */
+    private ?\DateTimeInterface $birthDate;
+
+    /**
+     * @ORM\Column(type="smallint")
+     */
+    private ?int $sex;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getFirstName(): ?string
+    {
+        return $this->firstName;
+    }
+
+    public function setFirstName(string $firstName): self
+    {
+        $this->firstName = $firstName;
+
+        return $this;
+    }
+
+    public function getLastName(): ?string
+    {
+        return $this->lastName;
+    }
+
+    public function setLastName(string $lastName): self
+    {
+        $this->lastName = $lastName;
+
+        return $this;
+    }
+
+    public function getBirthDate(): ?\DateTimeInterface
+    {
+        return $this->birthDate;
+    }
+
+    public function setBirthDate(\DateTimeInterface $birthDate): self
+    {
+        $this->birthDate = $birthDate;
+
+        return $this;
+    }
+
+    public function getSex(): ?int
+    {
+        return $this->sex;
+    }
+
+    public function setSex(int $sex): self
+    {
+        $this->sex = $sex;
+
+        return $this;
+    }
+}

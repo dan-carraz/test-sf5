ea68206a70acf05b410a0d0a5271af1039def170
Dan Carraz <dcarraz@lefigaro.fr>
2021-04-16 10:45:16 +0200

    add tests

---
+++ symfony/tests/Para1Test.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Tests;
+
+use PHPUnit\Framework\TestCase;
+
+class Para1Test extends TestCase
+{
+    public function test(): void
+    {
+        $this->assertTrue(true);
+    }
+}
